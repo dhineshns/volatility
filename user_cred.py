8b19873d8a79d88ff53975cf891969a281008276
dhineshns <dhineshns@gmail.com>
2018-02-17 14:04:09 -0600

    user_cred file created with dummy alues

---
+++ user_cred.py
@@ -0,0 +1,13 @@
+#### TradeKing Creds
+secrets = {"CONSUMER_KEY" : "####",
+           "CONSUMER_SECRET" : "####",
+           "ACCESS_KEY" : "####",
+           "ACCESS_SECRET" : "####"}
+
+##### Yahoo Creds
+mail_creds = {"SMTP_SERVER" : "smtp.mail.yahoo.com",
+              "SMTP_PORT" : 465,
+              "SMTP_USERNAME" : "####",
+              "SMTP_PASSWORD" : "####",
+              "EMAIL_FROM" : "####",
+              "EMAIL_TO" : "####"}
